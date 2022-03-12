5e8afac0f2f5d7b93dfe67ef39c5815cb44c502c
Nate Cater <nate.cater7@gmail.com>
2022-03-12 01:02:58 -0800

    added provided method

--- Battleship.java
+++ Battleship.java
@@ -6,6 +6,30 @@ public class Battleship {
 	
 		System.out.println("Welcome to Battleship!);
 		
+		Scanner input = new Scanner(System.in);
+		
+		
+		
 		//checking something
 	}
+	
+	// method provided for assignment to print the battleship map
+	
+	private static void printBattleship(char[][] player) {
+		System.out.print("  ");
+		for (int row = -1; row < 5; row++) {
+			if (row > -1) {
+				System.out.print(row + " ");
+			}
+			for (int column = 0; column < 5; column++) {
+				if (row == -1) {
+					System.out.print(column + " ");
+				} else {
+					System.out.print(player[row][column] + " ");
+				}
+			}
+			System.out.println("");
+		}
+	}
+	
 }
