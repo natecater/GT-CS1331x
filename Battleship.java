70c6a13943e9a26adc18e9c040e7e41557fc8e46
Nate Cater <nate.cater7@gmail.com>
2022-03-12 01:05:23 -0800

    fixed string left open

--- Battleship.java
+++ Battleship.java
@@ -4,7 +4,7 @@ public class Battleship {
 
 	public static void main(String[] args){
 	
-		System.out.println("Welcome to Battleship!);
+		System.out.println("Welcome to Battleship!");
 		
 		Scanner input = new Scanner(System.in);
 		
