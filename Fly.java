70da19cbd40f358860fa0f15ed4e9f1046cbf064
Nate Cater <nate.cater7@gmail.com>
2022-04-04 19:02:28 -0700

    updating pond project

--- Fly.java
+++ Fly.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 public class Fly {
 
 	// private instance variables
@@ -5,8 +7,8 @@ public class Fly {
 	private double speed;
 	
 	//public static variables
-	public static final int mass_default = 5;
-	public static final int speed_default = 10;
+	public static final double mass_default = 5.0;
+	public static final double speed_default = 10.0;
 	
 	//constructors
 	public Fly(double mass, double speed) {
@@ -14,7 +16,7 @@ public class Fly {
 		this.speed = speed;
 	}
 	
-	public Fly(mass) {
+	public Fly(double mass) {
 		this(mass, speed_default);
 	}
 	
@@ -30,24 +32,47 @@ public class Fly {
 	public double getSpeed() {
 		return speed;
 	}
+	
+	//setters or mutators
+	public void setMass(int newMass) {
+		this.mass = newMass;
+	}
+	
+	public void setSpeed(int newSpeed) {
+		this.speed = newSpeed;
+	}
 
 	//toString overriding
 	public String toString() {
+		
+		DecimalFormat df = new DecimalFormat("#.00");
+			
 		if (mass == 0) {
-			return "I'm dead, but I used to be a fly with a speed of " + speed + ".";
+			return "I'm dead, but I used to be a fly with a speed of " + df.format(speed) + ".";
 		}
 		else {
-			return "I'm a speedy fly with " + speed + "speed and " + mass + " mass.";
+			return "I'm a speedy fly with " + df.format(speed) + "speed and " + df.format(mass) + " mass.";
+		}
+	}
+	
+	//behaviors
+	public void grow(double added_mass) {
+		if (mass < 20) {
+			while (mass < 20) {
+				speed++;
+				added_mass--;
+				mass++;
+			}
+			mass += added_mass;
+		}
+		else {
+			speed += (double)(added_mass * 0.5);
+			mass += added_mass;
 		}
 	}
 	
 	public boolean isDead() {
-		if (mass == 0) {
-			return true;
-		}	
-		else {
-			return false;
-		}
+		return (mass == 0 ? true : false);
 	}
 	
 }
---
+++ Frog.java
@@ -0,0 +1,73 @@
+public class Frog {
+
+	//instance variables
+	private String name;
+	private int age;
+	private double tongueSpeed;
+	private boolean isFroglet;
+	protected String species = "Rare Pepe";
+	private int num_months;
+	
+	//static constants/variables
+	public static final int default_age = 5;
+	public static final double default_tongueSpeed = 5.0;
+	
+	//constructors
+	public Frog(String name, int age, double tongueSpeed) {
+		this.name = name;
+		this.age = age;
+		this.tongueSpeed = tongueSpeed;
+	}
+	
+	public Frog(String name, double ageInYears) {
+		this.name = name;
+		this.ageInYears = 
+	}
+	
+	public Frog(String name) {
+		this(name, default_age, default_tongueSpeed);
+	}
+	
+	//behavior methods
+	
+	public void grow(int num_months) {
+		if (age < 12) {
+			while (age < 12) {
+				age++;
+				num_months--;
+				tongueSpeed++;
+			}
+			tongueSpeed += num_months;
+		}
+		else if (age > 30) {
+			while (num_months > 0) {
+				age++;
+				num_months--;
+				if (tongueSpeed > 5) {
+					tongueSpeed--;
+				}
+			}
+		}
+		
+		if ((age > 1) && (age < 7)) {
+			isFroglet = true;
+		}
+		else {
+			isFroglet = false;
+		}
+	}
+	
+	public void grow() {
+		if (age < 12) {
+			tongueSpeed++;
+		}
+		else if (age > 30) {
+			if (tongueSpeed > 5) {
+				tongueSpeed--;
+			}
+		}
+		
+		age++;
+		
+	}
+}
